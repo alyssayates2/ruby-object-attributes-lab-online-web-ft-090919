c502cda79fc1726693ad3e5e590d8085815986f4
Alyssa Yates <bosslys@icloud.com>
2019-09-17 18:27:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
   attr_accessor :name, :breed
   def initialize(name, breed)
-    @name = name 
-    @breed = breed 
+    @name = name
+    @breed = breed
   end
 end
 
-dog1 = Dog.new 
+dog1 = Dog.new
 dog1.name = "Fido"
 dog1.breed = "Beagle"
